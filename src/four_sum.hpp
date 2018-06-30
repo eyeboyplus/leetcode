4ae95cb7ddb5b53b377e845c3c1147c613db7eee
eyeboy <eyeboyplus@163.com>
2018-06-30 15:29:35 +0800

    four sum

--- src/four_sum.hpp
+++ src/four_sum.hpp
@@ -6,6 +6,7 @@
 #define LEETCODE_FOUR_SUM_HPP
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,7 +27,47 @@ using namespace std;
  */
 namespace four_sum {
     vector<vector<int>> solution(vector<int> &nums, int target) {
+        vector<vector<int>> result;
 
+        int len = nums.size();
+
+        if(len < 4)
+            return result;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < len; ++i) {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int n1 = nums[i];
+            for(int j = i+1; j < len; j ++) {
+                if(j > i+1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int n2 = nums[j];
+                int start = j + 1;
+                int end = len - 1;
+                while(start < end) {
+                    int n3 = nums[start];
+                    int n4 = nums[end];
+
+                    int val = n1 + n2 + n3 + n4;
+                    if(val < target) {
+                        while(++start < end && nums[start] == n3);
+                    } else if(val > target) {
+                        while(--end > start && nums[end] == n4);
+                    } else {
+                        vector<int> vec = {n1, n2, n3, n4};
+                        result.push_back(vec);
+
+                        while(++start < end && nums[start] == n3);
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
 #endif //LEETCODE_FOUR_SUM_HPP
