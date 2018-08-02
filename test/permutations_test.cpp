d0c44b6d459c32a44e0e6f971cb022bb972c3017
eyeboy <eyeboyplus@163.com>
2018-08-02 16:25:40 +0800

    permutations

---
+++ test/permutations_test.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eyeboy on 2018/8/2.
+//
+
+#include "permutations.hpp"
+#include "gtest/gtest.h"
+
+#include <iostream>
+
+using namespace permutations;
+
+TEST(permutations, solution) {
+
+    vector<int> vec1 = {1, 2, 3};
+
+    vector<vector<int>> res1 = solution(vec1);
+
+    std::cout << "";
+}
